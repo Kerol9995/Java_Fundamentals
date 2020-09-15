5268a12a697cb90a070016e123b080cdb56635d9
Dmitriy <9995@tut.by>
2020-09-15 09:30:09 +0300

    Initial commit

---
+++ src/com/Kerol9995/LearnEpam.java
@@ -0,0 +1,25 @@
+package com.Kerol9995;
+
+import java.math.BigDecimal;
+
+public class Program {
+public static void main(String[] args) {
+        Cat sam = new SiameseCat(); //1
+        sam.zzz(); //2
+        }
+        }
+public abstract class Cat {
+    abstract void eat();
+    void zzz() {
+        System.out.println("Sleeps whole day");
+    }
+}
+class SiameseCat extends Cat {} /
+
+
+public class LearnEpam {
+    public static void main(String[] args){
+
+
+    }
+}
